24f5234bba21b2b159d86fadc6ede48d50c41d34
Filip Skoczeń <philipskoczen@gmail.com>
2022-03-16 20:03:51 +0100

    add dist

---
+++ assets/_...all_.cdefcd39.js
@@ -0,0 +1,1 @@
+import{e as n,k as o,b as s,t as a,l as r,o as c}from"./vendor.238515f8.js";var t={};const f=n({setup(l){const{t:e}=o();return(p,u)=>(c(),s("div",null,a(r(e)("not-found")),1))}});typeof t=="function"&&t(f);export{f as default};
